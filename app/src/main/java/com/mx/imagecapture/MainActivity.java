8d5fbe789e27b65f7d8f7e3029d41ff025f977a5
hectormoreno <hectoraguilas93@gmail.com>
2020-11-03 11:41:12 -0600

    inicio proyecto

---
+++ app/src/main/java/com/mx/imagecapture/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mx.imagecapture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String codeImageCapture = ""
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
